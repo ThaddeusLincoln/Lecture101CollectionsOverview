5a79eca0157356b010210854c862b4fceaaffe27
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-20 00:57:20 +0200

    Created a class Theater (with an inner class Seat, with a private constructor), which will be a base clase for the samples we want to try out using collections.

---
+++ Lect101CollectionsOverview/src/com/turing/jdev/collections/test/Main.java
@@ -0,0 +1,12 @@
+package com.turing.jdev.collections.test;
+
+import com.turing.jdev.collections.Theater;
+
+public class Main {
+
+	public static void main(String[] args){
+		Theater theater = new Theater("Olympian", 8, 12);
+		theater.getSeats();
+	}
+	
+}
